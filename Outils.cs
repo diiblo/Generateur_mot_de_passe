45a7cc26fede40b115e5a658be47d4d2a0125dc9
diiblo <kingrahman98@hotmail.com>
2025-02-17 01:01:14 +0100

    fin

---
+++ Outils.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+    static class Outils
+    {
+        public static int VerifNombrePositifNonNul(string question)
+        {
+            //while (true) //Autre Méthode pour vérifier si le nombre est positif ou non nul
+            //{
+            //    int reponse = DemanderNombre(question);
+            //    if (reponse > 0)
+            //    {
+            //        return reponse;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Erreur : Désolé votre nombre est négatif ou nul. Merci de recommencer.\n");
+            //    }
+            //}
+
+            return VerifIntervalleNombre(question, 1, int.MaxValue, "Le nombre doit être positif et non null");
+        }
+        public static int VerifIntervalleNombre(string question, int min, int max, string messageErreur = null)
+        {
+            while (true)
+            {
+                int reponse = DemanderNombre(question);
+                if (reponse >= min && reponse <= max)
+                {
+                    return reponse;
+                }
+                else
+                {
+                    if (messageErreur != null)
+                    {
+                        Console.WriteLine(messageErreur);
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Erreur : Désolé votre nombre est hors de l'intervalle {min} et {max}.\n");
+                        Console.WriteLine();
+
+                    }
+                }
+            }
+        }
+        public static int DemanderNombre(string question)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("\n---------");
+                Console.WriteLine("Générateur de mot de passe");
+                Console.WriteLine("---------\n");
+                Console.Write(question);
+
+                try
+                {
+                    int reponse = int.Parse(Console.ReadLine());
+                    return reponse;
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur : vous avez entré une mauvaise réponse.\n");
+                    Console.WriteLine();
+
+                }
+
+            }
+
+        }
+
+        public static void MotDePasse(string caracteres, int longueurMotdePasse, int nbrProposition)
+        {
+            Random rand = new Random();
+            
+            for (int i = 1; i <= nbrProposition; i++)
+            {
+                string motDePasse = "";
+                for (int y = 1; y <= longueurMotdePasse; y++)
+                {
+                    int index = rand.Next(0, caracteres.Length);
+                    motDePasse += caracteres[index];
+                }
+
+                Console.WriteLine($"\n{i}- Proposition : {motDePasse}");
+
+            }
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,59 @@
-﻿namespace Generateur_mot_de_passe
+﻿using FormationCS;
+namespace Generateur_mot_de_passe
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int NBR_PROPOSITIONS = 10;
+            //1-Longueur du mot de passe
+            int longueurMotdePasse = Outils.VerifNombrePositifNonNul("Quelle est la longueur du mot de passe voulu : ");
+
+
+            //2-Type de mot de passe
+
+            int choixType = Outils.VerifIntervalleNombre("Quel type de mot de passe voulez-vous ?\n" +
+                "1- Uniquement des caractère en miniscule\n" +
+                "2- Des caractères en minuscules et majuscules\n" +
+                "3- Des caractères et des chiffres\n" +
+                "4- Caractères, chiffres et caratères spéciaux\n" +
+                "Votre choix : ", 1, 4);
+
+            string alphabetLower = "abcdefghijklmnopqrstuvwxyz";
+            string alphabetUpper = alphabetLower.ToUpper();
+            string chiffres = "0123456789";
+            string caracteresSpeciaux = "!@#$%^&*()_+";
+
+            switch (choixType)
+            {
+                case 1:
+
+                    Outils.MotDePasse(alphabetLower, longueurMotdePasse, NBR_PROPOSITIONS);
+                    break;
+
+                case 2:
+
+                    Outils.MotDePasse((alphabetLower + alphabetUpper), longueurMotdePasse, NBR_PROPOSITIONS);
+                    break;
+
+                case 3:
+
+                    Outils.MotDePasse((alphabetLower + alphabetUpper + chiffres), longueurMotdePasse, NBR_PROPOSITIONS);
+                    break;
+
+                case 4:
+
+                    Outils.MotDePasse((alphabetLower + alphabetUpper + chiffres + caracteresSpeciaux), longueurMotdePasse, NBR_PROPOSITIONS);
+                    break;
+
+                default:
+                    Console.WriteLine("Erreur : Vous avez un numéro incorrecte.");
+                    break;
+            }
+
+
+
         }
     }
 }
